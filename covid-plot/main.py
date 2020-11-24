118ff3bbb3ef0c4c4dcd18f901f38733932ba57c
riccardoscheda <riccardo.scheda@gmail.com>
2020-11-24 15:41:53 +0100

    Added all the interventions

--- covid-plot/main.py
+++ covid-plot/main.py
@@ -20,6 +20,19 @@ from bokeh.plotting import figure
 from bokeh.sampledata.unemployment1948 import data
 from bokeh.transform import transform
 
+interventions = ['C1_School closing',
+               'C2_Workplace closing',
+               'C3_Cancel public events',
+               'C4_Restrictions on gatherings',
+               'C5_Close public transport',
+               'C6_Stay at home requirements',
+               'C7_Restrictions on internal movement',
+               'C8_International travel controls',
+               'H1_Public information campaigns',
+               'H2_Testing policy',
+               'H3_Contact tracing',
+               'H6_Facial Coverings']
+               
 DATA_URL = 'https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv'
 df = pd.read_csv(DATA_URL,
 		  parse_dates=['Date'],
@@ -109,7 +122,6 @@ df = pd.read_csv("covid-plot/data/data.csv",
 			        "RegionCode": str},
 			 error_bad_lines=False)
 			 
-interventions = list(df.columns[6:20])
 dates = list(df["Date"].astype(str).unique())
 options = list(np.unique(df["CountryName"]))
 select = Select(value="Italy", title='Country', options=options)
