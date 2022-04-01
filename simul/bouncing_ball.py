f553c9f80a51bbccca5afac102c18a65fe61663d
Mattia Ceccarelli <mattia.ceccarelli5@unibo.it>
2022-04-01 12:52:42 +0200

    add gravity

--- simul/bouncing_ball.py
+++ simul/bouncing_ball.py
@@ -2,7 +2,7 @@ from vpython import box, sphere, vector, color, rate, scene
 
 
 def build_box(side, thk, s2, s3):
-    wallR = box(pos=vector( side, 0, 0), size=vector(thk, s2, s3), color=color.red)
+    wallR = box(pos=vector(side, 0, 0), size=vector(thk, s2, s3), color=color.red)
     wallL = box(pos=vector(-side, 0, 0), size=vector(thk, s2, s3), color=color.red)
     wallB = box(pos=vector(0, -side, 0), size=vector(s3, thk, s3), color=color.blue)
     wallT = box(pos=vector(0,  side, 0), size=vector(s3, thk, s3), color=color.blue)
@@ -27,7 +27,7 @@ def main():
     # Ball creation with mass and momentum
     ball = sphere(color=color.green, radius=0.2, make_trail=False)
     ball.mass = 1.0
-    ball.p = vector(-0.15, -0.23, +0.27)
+    ball.v = vector(0.03, -0.05, 0.05)
 
     # Change side according to the thickness of the box
     side = side - thk * 0.5 - ball.radius
@@ -37,16 +37,19 @@ def main():
 
         rate(200) # Halt computation for 1/200s (it's the frequency of the animation)
 
+        a = vector(0, -.05, 0)
+
         # Equation
-        ball.pos = ball.pos + (ball.p / ball.mass) * dt
+        ball.pos = ball.pos + ball.v * dt + .5 * a * dt*dt
+        ball.v = ball.v + a * dt
 
         # Wall hitting check
         if not (side > ball.pos.x > -side):
-            ball.p.x = -ball.p.x
+            ball.v.x = -ball.v.x
         if not (side > ball.pos.y > -side):
-            ball.p.y = -ball.p.y
+            ball.v.y = -ball.v.y
         if not (side > ball.pos.z > -side):
-            ball.p.z = -ball.p.z
+            ball.v.z = -ball.v.z
 
 
 if __name__ == '__main__':
