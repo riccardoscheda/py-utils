68c8224d769e011ccbd7087829b32857ce65ea04
Mattia Ceccarelli <mattia.ceccarelli5@unibo.it>
2022-04-26 15:33:18 +0200

    clean code

--- simul/bouncing_ball.py
+++ simul/bouncing_ball.py
@@ -3,25 +3,9 @@
 
 import numpy as np
 from vpython import box, sphere, vector, color, rate, scene
+
 from physics import simplettic
-
-
-def build_box(side, thk, s2, s3):
-    wallR = box(pos=vector(side, 0, 0), size=vector(thk, s2, s3), color=color.red)
-    wallL = box(pos=vector(-side, 0, 0), size=vector(thk, s2, s3), color=color.red)
-    wallB = box(pos=vector(0, -side, 0), size=vector(s3, thk, s3), color=color.blue)
-    wallT = box(pos=vector(0,  side, 0), size=vector(s3, thk, s3), color=color.blue)
-    wallBK = box(pos=vector(0, 0, -side), size=vector(s2, s2, thk), color=color.gray(0.7))
-
-
-def check_wall_hit(ball: sphere, side: float):
-    # Wall hitting check
-    if not (side > ball.pos.x > -side):
-        ball.v.x = -ball.v.x
-    if not (side > ball.pos.y > -side):
-        ball.v.y = -ball.v.y
-    if not (side > ball.pos.z > -side):
-        ball.v.z = -ball.v.z
+from utils import build_box, check_wall_hit
 
 
 def create_balls(N: int=1) -> list:
