6041b3b97ae2210580de9538d4d18a4d04687fa4
Mattia Ceccarelli <mattia.ceccarelli5@unibo.it>
2022-05-25 12:57:12 +0200

    added usage example

--- src/ml/data_utils.py
+++ src/ml/data_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#%%
+
 import pandas as pd
 import numpy as np
 
@@ -30,6 +32,27 @@ def df_to_numpy(df: pd.DataFrame) -> np.array:
 
     return df.to_numpy().reshape(shape)
 
+#%%
 
 if __name__ == '__main__':
-    pass
+
+    # Usage example
+    import pandas as pd
+    import io
+
+    data = io.StringIO('''Fruit,Color,Count,Price
+                    Apple,Red,3,$1.29
+                    Apple,Green,9,$0.99
+                    Pear,Red,25,$2.59
+                    Pear,Green,26,$2.79
+                    Lime,Green,99,$0.39
+                    Lime,Red,,
+                    ''')
+
+    df_unindexed = pd.read_csv(data)
+    df = df_unindexed.set_index(['Fruit', 'Color'])
+    arr = df_to_numpy(df)
+# %%
+
+
+
